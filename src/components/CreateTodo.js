63fec9ea4d01507d48f71ed87b66805bf61ea8d8
nishantsapkota487 <63022262+nishantsapkota487@users.noreply.github.com>
2020-07-27 16:48:46 -0400

    Update CreateTodo.js

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import TaskRender from './TaskRender'
 
 const CreateTodo = () => {
-  const [ todo, setTodo] = useState(['nishant'])
+  const [ todo, setTodo] = useState([])
   const [ userInput, setUserInput] = useState('')
 
   const Change = (event) => {
